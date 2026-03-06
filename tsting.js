dd86918ebda637bb496a3f50be70c50134884faa
Kushagra Bharti <kushagrabharti04@gmail.com>
2026-03-06 22:41:29 +0530

    first commit

--- tsting.js
+++ tsting.js
@@ -1,9 +1,71 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
 
-function sumArray(arr) {
-  let total = 0;
-  for (let i = 0; i <= arr.length; i++) {
-    total = arr[i];
-  }
+const app = express()
 
-}
+app.use(express.json())
 
+mongoose.connect("mongodb://localhost:27017/test")
+
+const UserSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    role: String
+})
+
+const User = mongoose.model("User", UserSchema)
+
+app.post("/login", async (req, res) => {
+
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+        res.send("user not found")
+    }
+
+    if (user.password === password) {
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            "secret123"
+        )
+
+        res.json({
+            message: "login success",
+            token: token
+        })
+
+    } else {
+        res.send("wrong password")
+    }
+
+})
+
+app.get("/users", async (req, res) => {
+
+    const users = await User.find()
+
+    res.json(users)
+
+})
+
+app.post("/register", async (req, res) => {
+
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password,
+        role: "user"
+    })
+
+    await user.save()
+
+    res.send("user created")
+
+})
+
+app.listen(3000, () => {
+    console.log("server started")
+})
